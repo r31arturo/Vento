8a748a90815b842bda4dcbe0bdf89ea220de6402
r31arturo <r31arturo@gmail.com>
2018-07-18 11:10:38 -0300

    Commit de prueba

--- src/main/java/utils/ImprPant.java
+++ src/main/java/utils/ImprPant.java
@@ -38,25 +38,5 @@ public class ImprPant {
         FileUtils.copyFile(scrFile, new File(Constantes.Path_Captura+Test+" - Error.png"));
         Log.doLogging("Captura de Pantalla: "+Test+" - Error.png");
     }
-/*
-    public static void main(String[] args) throws Exception {
 
-        // set the geckodriver.exe property
-        System.setProperty("webdriver.gecko.driver", "C:/~/geckodriver.exe");
-        // open firefox
-        WebDriver driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
-        // open webpage
-        driver.get("http://chercher.tech/java/practice-pop-ups-selenium-webdriver");
-        // create object to robt class
-        Robot robot = new Robot();
-        // create rectangle for full screenshot
-        Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-        // capture screen of the desktop
-        BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
-        // save the screenshot to local system
-        ImageIO.write(screenFullImage, "png", new File("D:\\FullScreenshotRobot.png"));
-
-        System.out.println("Full Desktop screenshot saved!");
-    }*/
 }
